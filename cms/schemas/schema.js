039b0091efbdce48a3dd91ce1ced5ec4203d8143
joshua <hello.joshua@hotmail.com>
2022-10-06 23:59:57 -0400

    cms cleanup
    cms cleanup

--- cms/schemas/schema.js
+++ cms/schemas/schema.js
@@ -5,11 +5,6 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-
 import home from './home'
 import about from './about'
 import testimonial from './testimonial'
@@ -32,11 +27,5 @@ export default createSchema({
     testimonial,
     galleries,
     services
-    // post,
-    // author,
-    // category,
-    // // When added to this list, object types can be used as
-    // // { type: 'typename' } in other document schemas
-    // blockContent,
   ]),
 })
