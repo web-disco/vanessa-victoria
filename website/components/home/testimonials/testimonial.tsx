c9b464737c6193caa95f9dd4e2763691d7415c74
joshua <hello.joshua@hotmail.com>
2022-10-31 22:00:07 -0400

    updates
    updates

--- website/components/home/testimonials/testimonial.tsx
+++ website/components/home/testimonials/testimonial.tsx
@@ -5,7 +5,7 @@ interface TestimonailProps {
 
 const Testimonial = ({ name, testimonial }: TestimonailProps) => {
   return (
-    <div>
+    <div className="px-5">
       <p className="font-light italic text-offBlack mb-6 text-[14px] leading-7">
         {testimonial}
       </p>
