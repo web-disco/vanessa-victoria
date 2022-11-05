e29a06d592c162a4ce1b525d1e1442ed5b4f9cb9
joshua <hello.joshua@hotmail.com>
2022-11-05 18:46:18 -0400

    Update contact-form.tsx

--- website/components/contact/contact-form.tsx
+++ website/components/contact/contact-form.tsx
@@ -189,22 +189,20 @@ const ContactForm = () => {
     //     );
     //   }}
     // </Formik>
-    <form
-      name="Contact"
-      method="POST"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      className="grid grid-cols-2 gap-8"
-    >
-      <input type="hidden" name="form-name" value="Contact" />
-      <input
-        type="text"
-        name="firstName"
-        placeholder="First Name *"
-        className="w-full h-[40px] pb-2 bg-transparent border-b  border-sage text-brown placeholder-brown outline-none"
-        required
-      />
-      <button type="submit">Submit</button>
+    <form name="contact" data-netlify="true">
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   );
 };
