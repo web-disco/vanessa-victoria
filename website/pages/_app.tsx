20f9826090ee89249589b3957a5b90d09a012f09
joshua <hello.joshua@hotmail.com>
2022-12-06 20:43:07 -0500

    google analytics
    google analytics

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { NextComponentType } from "next";
 import { AppContext, AppInitialProps, AppLayoutProps } from "next/app";
+import Script from "next/script";
 import { LayoutGroup } from "framer-motion";
 import dynamic from "next/dynamic";
-import { CookiesProvider, useCookies } from "react-cookie";
+import { CookiesProvider } from "react-cookie";
 import { ReactNode } from "react";
 
 import "../styles/globals.css";
@@ -21,11 +22,26 @@ const MyApp: NextComponentType<AppContext, AppInitialProps, AppLayoutProps> = ({
     Component.getLayout || ((page: ReactNode) => <Page>{page}</Page>);
 
   return (
-    <LayoutGroup>
-      <CookiesProvider>
-        {getLayout(<Component {...pageProps} />)}
-      </CookiesProvider>
-    </LayoutGroup>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+          `}
+      </Script>
+      <LayoutGroup>
+        <CookiesProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </CookiesProvider>
+      </LayoutGroup>
+    </>
   );
 };
 
